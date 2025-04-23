de7899af06d33722abbb2f17c6266a4587a23e10
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2025-04-23 18:54:58 -0400

    Add compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.compose.compiler) apply false
     id("com.autonomousapps.dependency-analysis") version "2.16.0"
 }
 true
